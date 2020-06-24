7f7a444e0ff40fa3f2e1c6d44cebf00d327c2af1
stjohn <52610738+sequent5@users.noreply.github.com>
2020-06-24 15:50:36 +0800

    Update passwords.php

--- resources/lang/cn/passwords.php
+++ resources/lang/cn/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => '密码设置应至少含6位字符，并和确认密码保持一致.',
+    'reset' => '密码已重置!',
+    'sent' => '密码重置链接已发送到你指定的电子邮箱!',
+    'token' => '此密码重置令牌无效.',
+    'user' => "系统无此电子邮件帐户用户.",
 
 ];
